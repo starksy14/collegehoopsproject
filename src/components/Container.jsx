8061abdd822f70273cee5f1cbab071c3e96cfc14
Greg Stark <starksy@gmail.com>
2023-12-13 23:47:00 -0600

    Add files via upload

---
+++ src/components/Container.jsx
@@ -0,0 +1,76 @@
+import './App.css';
+import Wooden from './components/Wooden';
+import Lewis from './components/Lewis';
+import Calhoun from './components/Calhoun';
+import Olson from './components/Olson';
+import Tarkanian from './components/Tarkanian';
+import McGuire from './components/McGuire';
+import Allen from './components/Allen';
+import Richardson from './components/Richardson';
+import Krzyzewski from './components/Krzyzewski';
+import Mears from './components/Mears';
+import Carnesecca from './components/Carnesecca';
+import Vitale from './components/Vitale';
+import Sutton from './components/Sutton';
+import Maravich from './components/Maravich';
+import Chaney from './components/Chaney';
+import Smith from './components/Smith';
+import Haskins from './components/Haskins';
+import Meyer from './components/Meyer';
+import Wright from './components/Wright';
+import Fisher from './components/Fisher';
+import Rupp from './components/Rupp';
+import Izzo from './components/Izzo';
+import Carril from './components/Carril';
+import Valvano from './components/Valvano';
+import Carlesimo from './components/Carlesimo';
+import Crum from './components/Crum';
+import Williams from './components/Williams';
+import Kruger from './components/Kruger';
+import Henson from './components/Henson';
+import Iba from './components/Iba';
+
+const Container = () => {
+
+  return (
+    <>
+    <h1>Untitled College Basketball Simulation Project</h1>
+    <div><h4>kajhdkahfdkashdfkj</h4>
+      <div className="container">
+      <div className="item"><Wooden/></div>
+      <div className="item"><Lewis/></div>
+      <div className="item"><Calhoun/></div>
+      <div className="item"><Olson/></div>
+      <div className="item"><Tarkanian/></div>
+      <div className="item"><McGuire/></div>
+      <div className="item"><Allen/></div>
+      <div className="item"><Richardson/></div>
+      <div className="item"><Krzyzewski/></div>
+      <div className="item"><Mears/></div>
+      <div className="item"><Carnesecca/></div>
+      <div className="item"><Vitale/></div>
+      <div className="item"><Sutton/></div>
+      <div className="item"><Maravich/></div>
+      <div className="item"><Chaney/></div>
+      <div className="item"><Smith/></div>
+      <div className="item"><Haskins/></div>
+      <div className="item"><Meyer/></div>
+      <div className="item"><Wright/></div>
+      <div className="item"><Fisher/></div>
+      <div className="item"><Rupp/></div>
+      <div className="item"><Izzo/></div>
+      <div className="item"><Carril/></div>
+      <div className="item"><Valvano/></div>
+      <div className="item"><Carlesimo/></div>
+      <div className="item"><Crum/></div>
+      <div className="item"><Williams/></div>
+      <div className="item"><Kruger/></div>
+      <div className="item"><Henson/></div>
+      <div className="item"><Iba/></div>
+</div>
+</div>
+    </>
+  )
+}
+
+export default Container;
