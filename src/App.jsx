a556e58e3bfbf7e5751792b480e46130c2bc675f
Greg Stark <starksy@gmail.com>
2023-12-13 23:38:29 -0600

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,21 @@ import Vitale from './components/Vitale';
 import Sutton from './components/Sutton';
 import Maravich from './components/Maravich';
 import Chaney from './components/Chaney';
+import Smith from './components/Smith';
+import Haskins from './components/Haskins';
+import Meyer from './components/Meyer';
+import Wright from './components/Wright';
+import Fisher from './components/Fisher';
+import Rupp from './components/Rupp';
+import Izzo from './components/Izzo';
+import Carril from './components/Carril';
+import Valvano from './components/Valvano';
+import Carlesimo from './components/Carlesimo';
+import Crum from './components/Crum';
+import Williams from './components/Williams';
+import Kruger from './components/Kruger';
+import Henson from './components/Henson';
+import Iba from './components/Iba';
 
 const App = () => {
 
@@ -37,21 +52,21 @@ const App = () => {
       <div className="item"><Sutton/></div>
       <div className="item"><Maravich/></div>
       <div className="item"><Chaney/></div>
-      <div className="item"><Olson/></div>
-      <div className="item"><McGuire/></div>
-      <div className="item"><Allen/></div>
-      <div className="item"><Richardson/></div>
-      <div className="item"><Krzyzewski/></div>
-      <div className="item"><Mears/></div>
-      <div className="item"><Carnesecca/></div>
-      <div className="item"><Sutton/></div>
-      <div className="item"><Wooden/></div>
-      <div className="item"><Lewis/></div>
-      <div className="item"><Olson/></div>
-      <div className="item"><McGuire/></div>
-      <div className="item"><Allen/></div>
-      <div className="item"><Richardson/></div>
-      <div className="item"><Krzyzewski/></div>
+      <div className="item"><Smith/></div>
+      <div className="item"><Haskins/></div>
+      <div className="item"><Meyer/></div>
+      <div className="item"><Wright/></div>
+      <div className="item"><Fisher/></div>
+      <div className="item"><Rupp/></div>
+      <div className="item"><Izzo/></div>
+      <div className="item"><Carril/></div>
+      <div className="item"><Valvano/></div>
+      <div className="item"><Carlesimo/></div>
+      <div className="item"><Crum/></div>
+      <div className="item"><Williams/></div>
+      <div className="item"><Kruger/></div>
+      <div className="item"><Henson/></div>
+      <div className="item"><Iba/></div>
 </div>
 </div>
     </>
